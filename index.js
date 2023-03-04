7548ef9c92a8541e71f5c9ce07a691f49671de8d
MiniKrakenBot <minikrakenbot@gmail.com>
2023-03-04 01:12:46 -0300

    ata

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ let mainWindow;
 app.on('ready', () => {
     
     mainWindow = new BrowserWindow({
+        center: true,
+        closable: false,
         minHeight: 600,
         minWidth: 1200,
         //frame: false,
         titleBarStyle: "Kraken Boot DB",
         titleBarOverlay: true,
+        
     });
     mainWindow.setMenu(null);
     mainWindow.setIcon(`${__dirname}/icon.png`, `Overlay Icon`)
+    mainWindow.setBackgroundColor('#2d2c38')
+    //mainWindow.webContents.openDevTools()
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
